23785bdf8583e932781a123553e52c2b8116b781
marcela griet <marcelagriet@gmail.com>
2021-07-29 21:32:57 +0000

    Merged in feature/OT52-20 (pull request #8)
    Feature/OT52-20
    
    * Setup Alert
    
    * Setup Alert as reusable components
    
    * Corrections to PR #1
    
    * Alert component modifications with reusablity purpose
    
    * Merge remote-tracking branch 'origin/develop' into feature/OT52-20
    
    
    Approved-by: Dario Velez

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Alert from './shared/Alert/Alert';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import testApp from './shared/testApp';
 import testNotFound from './shared/testNotFound';
@@ -6,14 +10,83 @@ import './App.css';
 import Loader from './shared/Loader/Loader';
 
 function App() {
+
+  const [showSuccess, setShowSuccess] = useState(false)
+  const [showError, setShowError] = useState(false)
+  const [showInfo, setShowInfo] = useState(false)
+ 
   return (
-    <BrowserRouter>
-      <Loader/>
-      <Switch>
-        <Route exact path="/" component={testApp}/>
-        <Route component={testNotFound}/>
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+        < button className="button" onClick= {()=>setShowError(true)}> Error Button </button>
+        <Alert 
+          show= {showError}
+          title='Oops'
+          text= 'Something went wrong'
+          type = 'error' 
+          onConfirm={()=>setShowError(false)}
+        />
+        < button className="button" onClick= {()=>setShowSuccess(true)}> Success Button </button>
+        <Alert 
+          show= {showSuccess}
+          title='Good Job!'
+          text= 'You clicked the button'
+          type = 'success' 
+          onConfirm={()=>setShowSuccess(false)}
+        />
+        < button className="button" onClick= {()=>setShowInfo(true)}> Info Button </button>
+        <Alert 
+          show= {showInfo}
+          title='Info'
+          text= 'You clicked the button'
+          type = 'info' 
+          onConfirm={()=>setShowInfo(false)}
+        />
+      </header>
+    </div>
   );
 }
 
--- src/shared/Alert/Alert.jsx
+++ src/shared/Alert/Alert.jsx
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
-import { withSwalInstance } from 'sweetalert2-react';
-import swal from 'sweetalert2';
+import React from 'react';
+import SweetAlert from 'sweetalert2-react';
 import './Alert.css';
 
-const SweetAlert = withSwalInstance(swal);
- 
 const Alert = (props) => {
-
-  const [show, setShow] = useState(false)
-  
-  const onClickAlert = () => {
-    setShow(true)
-  }
-  const onConfirmAlert = () => {
-    setShow(false)
-  }
 
  return (
     <div>
-      <button  className='button' onClick={onClickAlert} style={{marginLeft:'5px'}}> {props.buttonText} </button>
       <SweetAlert
-        show= {show}
+        show= {props.show}
         title ={props.title}
         text= {props.text}
         type= {props.type} //FYI: Choose between 4 built-in icons: 'warning', 'error', 'success' or 'info' 
-        onConfirm={onConfirmAlert}
-        buttonText = {props.buttonText}
+        onConfirm={props.onConfirm}
       />
     </div>
   )
