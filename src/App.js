1975d2c1938cfd57dc8236dc885a848d476ae8a8
Ignacio Peralta <nachoperalta2806@gmail.com>
2021-07-29 13:40:17 +0000

    Merged in release-feature/OT52-19 (pull request #5)
    Release Loader feature
    
    Approved-by: Dario Velez

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Header } from './shared/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Loader from './shared/Loader/Loader';
+
 function App() {
 
   return (
     <BrowserRouter>
+      <Loader/>
       <Switch>
         <Route exact path='/' component={Header} />
       </Switch>
---
+++ src/shared/Loader/Loader.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Spinner from 'react-loader-spinner'
+import styles from '../../features/counter/Counter/module.css'
+ 
+const Loader = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  const stateLoading = () => {
+    setLoading(true);
+    setTimeout(() => {
+        setLoading(false);
+    }, 5000);
+  }
+
+  if(loading){
+      return <Spinner 
+        type="ThreeDots"
+        color="#00BFFF"
+        height={50}
+        width={50}/>
+  }
+  else{
+    return (
+    <div>
+      <button className={styles.button} onClick={stateLoading} style={{marginLeft:'4px'}}>Loading Component</button>
+    </div>
+  )
+  }  
+}
+
+export default Loader
