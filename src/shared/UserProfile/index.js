0b0988b806b86da7be92ed9e559e31c534646303
LazaroFMD <lazaronazareno@gmail.com>
2021-08-05 00:00:38 +0000

    Merged in feature/OT52-26 (pull request #16)
    Feature/OT52-26
    
    * userProfile.js and styles.js added
    
    * Merge remote-tracking branch 'origin/develop' into feature/OT52-26
    
    * button eliminar cuenta onclick alert
    
    
    Approved-by: Dario Velez

---
+++ src/shared/UserProfile/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Image, Button, ListGroup } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './styles.scss'
+
+const user = {
+	name: 'Mark',
+	surName : 'Zuckerberg',
+	mail: 'mz-facebook@gmail.com'
+}
+
+function UserProfile() {
+	return (
+		<Container className="">
+			<Container className="yellow p-0">
+				<Image className="referenceSize blue translate" src='https://image.flaticon.com/icons/png/512/848/848043.png' roundedCircle />
+			</Container>
+			<ListGroup className="translate" variant="flush">
+				<ListGroup.Item className="fs-1 text-break">Nombre: {user.name}</ListGroup.Item>
+				<ListGroup.Item className="fs-1 text-break">Apellido: {user.surName}</ListGroup.Item>
+				<ListGroup.Item className="fs-1 text-break">Email: {user.mail}</ListGroup.Item>
+			</ListGroup>
+			<Container className="translate d-flex justify-content-evenly">
+				<Button as={Link} to='/editUser' className="btn-info" size="lg">Editar Datos</Button>
+				<Button variant="danger" onClick={() => alert('Cuenta Eliminada(añadir accion en el futuro)')} size="lg">Eliminar Cuenta</Button>
+			</Container>
+		</Container>
+	)
+}
+
+export default UserProfile
