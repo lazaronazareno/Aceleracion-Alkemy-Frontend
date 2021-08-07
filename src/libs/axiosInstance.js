c9067238264366403aa2c999e35fa5be03aabd19
Orlando Arciniegas <orlandoarciniegas1508@gmail.com>
2021-08-07 23:19:56 +0000

    Merged in OT52-31 (pull request #18)
    Created an instance of axios to manage http requests
    
    * Created form to login
    
    * Created a basic form to login users in the app.
    
    * Created an instance of axios to manage http requests
    
    * Updated code review comments
    
    * Agregue config por default para peticiones post
    
    * Instancia de axios reutilizable
    
    * Solucionados conflictos merge
    
    
    Approved-by: Dario Velez

---
+++ src/libs/axiosInstance.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+const access_token = localStorage.getItem('token')
+
+axios.defaults.baseURL = process.env.REACT_APP_baseURL
+
+const headers = {'Authorization' : `Bearer ${access_token}`}
+
+
+const useAxios = ({ url, method, body = null }) => {
+
+	const [response, setResponse] = useState(null)
+
+	const [error, setError] = useState('')
+
+	const [loading, setloading] = useState(true)
+
+
+	const fetchData = () => {
+
+		axios[method](url, JSON.parse(headers), JSON.parse(body))
+
+			.then((res) => {
+
+				setResponse(res.data)
+
+			})
+
+			.catch((err) => {
+
+				setError(err)
+
+			})
+
+			.finally(() => {
+
+				setloading(false)
+
+			})
+
+	}
+
+
+	useEffect(() => {
+
+		fetchData()
+
+	}, [method, url, body, headers])
+
+
+	return { response, error, loading }
+
+}
+
+
+export default useAxios
+
+
+// FYI: asi se debe usar en el component que lo consuma para hacer requests al backend
+
+
+// const { response, loading, error } = useAxios({
+
+//   method: 'post',
+
+//   url: '/posts',
+
+//   body: JSON.stringify({
+
+//       userId: 1,
+
+//       id: 19392,
+
+//       title: 'title',
+
+//       body: 'Sample text',
+
+//   }),
+
+// });
+
+// const [data, setData] = useState([]);
+
+
+// useEffect(() => {
+
+//   if (response !== null) {
+
+//       setData(response);
+
+//   }
+
+// }, [response]);
