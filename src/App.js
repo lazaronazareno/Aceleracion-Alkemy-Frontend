365c1a08c1386225e5ee9f2c32f04c6d2f35ca45
Ignacio Peralta <nachoperalta2806@gmail.com>
2021-08-13 23:25:46 +0000

    Merged in feature/OT52-104 (pull request #26)
    Navigation transition
    
    * navigation transition
    
    * fix merge
    
    * fix details
    
    
    Approved-by: LazaroFMD
    Approved-by: Dario Velez
    Approved-by: marcela griet
    Approved-by: Orlando Arciniegas

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import { NewList } from './components/NewsList'
 import Login from './components/Login/Form'
 import Footer from './shared/Footer'
 import Register from './components/Register'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+
 import NewsDetails from './components/NewsDetails'
 import EditUser from './components/editUser/editUser'
 import UserProfile from './components/UserProfile'
 import actions from './redux/actions'
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import axios from 'axios'
 import FormNews from './components/NewsForm'
 
 function App() {
 	const dispatch = useDispatch()
-	const {addAuth, addUser} = actions
+	const { addAuth, addUser } = actions
 
 	const getAuthUser = async () => {
 		const user = await axios.get('http://localhost:4000/auth/me', {
@@ -30,7 +32,7 @@ function App() {
 			FYI: Here we need sent the request with axios instance.
 			For test I use axios dependency directly
 		*/
-		
+
 		dispatch(addUser(user.data))
 	}
 
@@ -43,20 +45,35 @@ function App() {
 		<div className="App">
 			<BrowserRouter>
 				<Header />
-				<Switch>
-					<Route exact path="/" >
-						<Home text='Mensaje de bienvenida de Prueba'/>
-					</Route>
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/register' component={Register} />
-					<Route exact path="/test" component={FormNews} />
-					<Route exact path="/novedades" component={NewList} />
-					<Route exact path="/novedades/:id" component={NewsDetails} />
-					<Route exact path="/editUser" component={EditUser} />
-					<Route exact path="/user" component={UserProfile} />
-					<Route exact path='/register' component={Register} />
-					<Route component={NotFound}/>
-				</Switch>
+				<Route
+					render={({ location }) => (
+						<TransitionGroup >
+							<CSSTransition
+								appear={true}
+								key={location.key}
+								timeout={{ enter: 600, exit: 400 }}
+								classNames="fade"
+							>
+								<div>
+									<Switch>
+										<Route exact path="/" >
+											<Home text='Mensaje de bienvenida de Prueba' />
+										</Route>
+										<Route exact path='/login' component={Login} />
+										<Route exact path='/register' component={Register} />
+										<Route exact path="/test" component={FormNews} />
+										<Route exact path="/novedades" component={NewList} />
+										<Route exact path="/novedades/:id" component={NewsDetails} />
+										<Route exact path="/editUser" component={EditUser} />
+										<Route exact path="/user" component={UserProfile} />
+										<Route exact path='/register' component={Register} />
+										<Route component={NotFound} />
+									</Switch>
+								</div>
+							</CSSTransition>
+						</TransitionGroup>
+					)}
+				/>
 				<Footer />
 			</BrowserRouter>
 		</div>
