707b562c6bef9121dbdde073bb7bd51e1f6a321d
marcela griet <marcelagriet@gmail.com>
2021-08-26 00:02:57 +0000

    Merged in feature/OT52-74 (pull request #53)
    Conexion de formulario de contacto con endpoint POST contacts
    
    * Conexion de formulario de contcato con endpoint
    
    
    Approved-by: LazaroFMD
    Approved-by: Dario Velez
    Approved-by: Tomás Ezequiel Rolón
    Approved-by: Ignacio Peralta

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React , { useEffect,useState }from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
-import { Button } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
+import useAxios from '../../libs/axiosInstance'
+import Loader from '../../shared/Loader/Loader'
+import Alert from '../../shared/Alert/Alert'
 
 function Contact() {
+	
+	const [showSucces, setShowSuccess] = useState(false)
+
 	const initialValues = {
 		name: '',
 		lastName: '',
@@ -17,82 +23,108 @@ function Contact() {
 		email: Yup.string().email('Formato invalido').required('Requerido'),
 		message: Yup.string().required('Requerido'),
 	})
+	
+	const httpConfig = {
+		url: '/contacts',
+		method: 'post'
+	}
+
+	const { response, error, loading, fetchData } = useAxios()
 
 	const onSubmit = (values, onSubmitProps) => {
-		console.log(values)
-		//FYI agregar el endpoint de contacto
+		const body = values
+		fetchData({ url: httpConfig.url, method: httpConfig.method, body })
 		onSubmitProps.resetForm()
 	}
+		
+	useEffect(() => {
+		if (!loading && response ) {
+			setShowSuccess(true)
+		}
+	}, [loading, response, error])
+
 	return (
-		<Formik
-			initialValues={initialValues}
-			validationSchema={validationSchema}
-			onSubmit={onSubmit}
-		>
-			<Form className="container d-flex flex-column text-dark border border-dark mt-2 p-5">
-				<h1 className="d-flex m-3">Contactate con nosotros</h1>
-				<div className="d-flex">
-					<div className="form-floating m-3 w-50">
-						<Field
-							type="text"
-							className="form-control"
-							id="floatingName"
-							name="name"
-							placeholder="nombre"
-						/>
-						<label htmlFor="floatingName">Nombre</label>
-						<span className="text-danger d-flex fs-4">
-							<ErrorMessage name="name" />
-						</span>
-					</div>
-					<div className="form-floating m-3 w-50">
-						<Field
-							type="text"
-							className="form-control"
-							id="floatingLastName"
-							name="lastName"
-							placeholder="lastName"
-						/>
-						<label htmlFor="floatingLastName">Apellido</label>
-						<span className="text-danger d-flex fs-4">
-							<ErrorMessage name="lastName" />
-						</span>
-					</div>
-				</div>
-				<div className="form-floating m-3">
-					<Field
-						type="text"
-						className="form-control"
-						id="floatingEmail"
-						name="email"
-						placeholder="email"
-					/>
-					<label htmlFor="floatingEmail">Email</label>
-					<span className="text-danger d-flex fs-4">
-						<ErrorMessage name="email" />
-					</span>
-				</div>
-				<div className="form-floating m-3">
-					<Field
-						component="textarea"
-						type="text"
-						className="form-control h-75 py-5"
-						id="floatingMessage"
-						name="message"
-						placeholder="message"
-					/>
-					<label htmlFor="floatingMessage">Escribe tu consulta...</label>
-					<span className="text-danger d-flex fs-4">
-						<ErrorMessage name="message" />
-					</span>
-				</div>
-				<div className="d-flex m-3">
-					<Button className="p-4 w-25" size="lg" type="submit">
+		<Container>
+			{loading ?
+				<Loader /> :
+				<Formik
+					initialValues={initialValues}
+					validationSchema={validationSchema}
+					onSubmit={onSubmit}
+				>
+					<Form className="container d-flex flex-column text-dark border border-dark mt-2 p-5">
+						<h1 className="d-flex m-3">Contactate con nosotros</h1>
+						<div className="d-flex">
+							<div className="form-floating m-3 w-50">
+								<Field
+									type="text"
+									className="form-control"
+									id="floatingName"
+									name="name"
+									placeholder="nombre"
+								/>
+								<label htmlFor="floatingName">Nombre</label>
+								<span className="text-danger d-flex fs-4">
+									<ErrorMessage name="name" />
+								</span>
+							</div>
+							<div className="form-floating m-3 w-50">
+								<Field
+									type="text"
+									className="form-control"
+									id="floatingLastName"
+									name="lastName"
+									placeholder="lastName"
+								/>
+								<label htmlFor="floatingLastName">Apellido</label>
+								<span className="text-danger d-flex fs-4">
+									<ErrorMessage name="lastName" />
+								</span>
+							</div>
+						</div>
+						<div className="form-floating m-3">
+							<Field
+								type="text"
+								className="form-control"
+								id="floatingEmail"
+								name="email"
+								placeholder="email"
+							/>
+							<label htmlFor="floatingEmail">Email</label>
+							<span className="text-danger d-flex fs-4">
+								<ErrorMessage name="email" />
+							</span>
+						</div>
+						<div className="form-floating m-3">
+							<Field
+								component="textarea"
+								type="text"
+								className="form-control h-75 py-5"
+								id="floatingMessage"
+								name="message"
+								placeholder="message"
+							/>
+							<label htmlFor="floatingMessage">Escribe tu consulta...</label>
+							<span className="text-danger d-flex fs-4">
+								<ErrorMessage name="message" />
+							</span>
+						</div>
+						<div className="d-flex m-3">
+							<Button className="p-4 w-25" size="lg" type="submit">
               Enviar
-					</Button>
-				</div>
-			</Form>
-		</Formik>
+							</Button>
+						</div>
+					</Form>
+				</Formik>
+			}	
+			<Alert 
+				show={showSucces} 
+				title={''} 
+				text={'Usuario creado exitosamente'} 
+				type={'success'} 
+				onConfirm={()=>setShowSuccess(false)}
+			/>
+		</Container>
 	)
 }
 
